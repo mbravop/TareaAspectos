7a5b08440d7db8df6251b042773c94ea874d7a87
K3vR0ld4n <107887773+K3vR0ld4n@users.noreply.github.com>
2022-11-05 23:15:45 -0500

    Implementación de Singleton AOP

---
+++ Tarea_SingletonAOP/src/ServiciosExt.java
@@ -0,0 +1,18 @@
+
+public class ServiciosExt {
+
+	private String nombre;
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public ServiciosExt(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public ServiciosExt() {}
+	
+	
+	
+}
